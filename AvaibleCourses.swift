032fa7aed0be0da889b64289df6a53a1356f7fcc
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-11-13 07:30:53 -0600

    Updated AvaibleCourses.swift (MVP)

--- AvaibleCourses.swift
+++ AvaibleCourses.swift
@@ -1,5 +1,5 @@
 //
-//  AvaibleCourses.swift
+//  ContentView.swift
 //  Profile
 //
 //  Created by Jayce Sagvold on 11/12/22.
@@ -7,44 +7,68 @@
 
 import SwiftUI
 
-struct AvaibleCourses: View {
+struct ContentView: View {
+    @State private var shouldShowDetail: Bool = false
     var body: some View {
-        List {
-            Image("mt-1")
-                .resizable()
-                .frame(height: 155.5)
-                .cornerRadius(15.5)
-            Text("C++ Beginner Course 2022")
+        VStack {
+            Text("User: j.Sagvold28")
                 .bold()
-                .font(.title2)
-            Text("Time: 120 miniutes 25 seconds")
-            Text("Price: $106.42 (including tax)")
-                .foregroundColor(.green)
+                .font(.title)
+                .padding(.all)
             
-            Image("mt-2")
-                .resizable()
-                .frame(height: 155.5)
-            Text("Quanitive Apitude Interview Prep Questons (50)")
-                .font(.title2)
-                .bold()
-            Text("Time: Self-Paced")
-            Text("Price: FREE (was 74.44 returning to that price in 2 days")
-                .foregroundColor(.green)
-            
-            Image("mt-8")
-                .resizable()
-                .frame(height: 155.5)
-            Text("Computer Networking Server Side - Python (editon 1)")
-                .font(.title2)
-                .bold()
-            
+            VStack {
+                Text("Fallowers: 20")
+                    .padding(.horizontal, 40)
+                Text("Fallowing: 6")
             }
-
-        }
-    }
-
-struct AvaibleCourses_Previews: PreviewProvider {
+            
+            VStack {
+                Text("Purchased Courses:")
+                    .font(.title3)
+                    .bold()
+            }
+            
+            HStack {
+                List {
+                    Image("mt-8")
+                        .resizable()
+                        .frame(height: 155.5)
+                        .cornerRadius(15.25)
+                    Text("Computer Networking Server Side - Python (edtion 1)")
+                        .bold()
+                    Text("$150.99 (including tax)")
+                        .foregroundColor(.green)
+                        .bold()
+                        .font(.headline)
+                    Image("mt-2")
+                        .resizable()
+                        .frame(height: 155.5)
+                        .cornerRadius(15.25)
+                    Text("Quantitave Apitude Interview Prep Questons (50)")
+                        .bold()
+                    Text("74.44 (including tax)")
+                        .foregroundColor(.green)
+                        .bold()
+                        .font(.headline)
+                    Image("mt-1")
+                        .resizable()
+                        .frame(height: 155.5)
+                        .cornerRadius(15.25)
+                    Text("C++ Beginner course (120 mins)")
+                        .bold()
+                    Text("$106.42 (including tax)")
+                        .foregroundColor(.green)
+                        .bold()
+                        .font(.headline)
+                        }
+                    }
+                        
+                    }
+                }
+            }
+    
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        AvaibleCourses()
+        ContentView()
     }
 }
